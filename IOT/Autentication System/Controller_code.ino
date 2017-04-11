6769e811e57d71d175f51293fc7557a5ceed1c53
mitsuan <sumit.cooldude00@gmail.com>
2017-04-11 16:22:21 +0530

    Create Controller_code.ino

---
+++ IOT/Autentication System/Controller_code.ino
@@ -0,0 +1,82 @@
+#include <Ethernet.h>
+#include<SPI.h>
+
+//device configuration
+byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+EthernetServer server = EthernetServer(12358);
+IPAddress ip(192, 168, 0, 102);
+//IPAddress ip(192, 168,100,200);
+
+int endGet=0;
+
+void setup() {
+  
+  Serial.begin(9600);
+  boolean receiving = false;
+  
+  Ethernet.begin(mac,ip); 
+  Serial.print("LocalIP address of the arduino:");
+  Serial.println(Ethernet.localIP());
+
+  
+  server.begin();
+  Serial.println("Server Started");
+  
+}
+
+void loop() {
+  
+  Serial.println("Waiting for a client to connect");
+  EthernetClient client = server.available();
+
+if(client) {
+  
+
+  Serial.print("Connected to a client : ");
+
+  String getReq="";
+
+  while(client.available()) {             
+    
+      char c = client.read();
+       Serial.print(c);
+
+
+        if(endGet==0)
+        {
+          if(c=='H')
+         {
+          endGet=1;
+
+          Serial.print("GET request: ");
+          Serial.println(getReq.substring(5));
+         }
+         else
+         {
+           getReq+=(char)c;
+
+         }
+         
+        }
+       
+       
+       
+      
+  }
+  
+  if(client.connected()) {
+      Serial.println("Response Sent to Client: A HTML Page");
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/html\n");
+      client.print("<center><h2>Robotics society welcomes you!!!</h2></center>");
+  }  
+  
+delay(5);
+client.stop();
+Serial.println("Client is disconnected");
+}
+
+delay(2000); // Wait for 2 seconds before starting to listen again
+}
+
+
